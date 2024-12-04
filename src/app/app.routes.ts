52a3ea9d475d7e584776b1544fa65262dcee895c
shamal20186010 <shamalwassana78@gmail.com>
2024-12-04 15:03:11 +0530

    setup routing and create dashbord

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { ManagCustomerComponent } from './manag-customer/manag-customer.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
 export const routes: Routes = [
     {
+        path: "",
+        component: DashbordComponent
+    },
+     {
         path: "manage-customer",
         component: ManagCustomerComponent
     }
